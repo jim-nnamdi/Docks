332b0687baece286c3366badcf462fd357d51b28
Jim <jimsamuel50@gmail.com>
2025-09-04 22:47:05 +0100

    entry point . servers

---
+++ constants.h
@@ -0,0 +1,16 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#include <stdio.h>
+#include <string.h>
+
+int ts = 1;
+int max_peers =  10;
+extern const char *socket_err = "socket error";
+extern const char *bind_err = "bind error";
+extern const char *lis_err = "listen error";
+extern const char *acc_err = "accept error";
+extern const char *read_err = "read error";
+extern const char *write_err = "write error";
+
+#endif
---
+++ server.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <sys/event.h>
+#include <netinet/in.h>
+
+#include "constants.h"
+
+void error(const char* msg);
+int serve(int p) {
+    int s, c;
+    size_t r, w;
+    struct sockaddr_in sv, cv;
+    socklen_t svz, cvz;
+    char buf[BUFSIZ];
+
+    s = socket(AF_INET, SOCK_STREAM, 0);
+    if (s < 0 ) error(socket_err);
+    
+    int opt = 1;
+    setsockopt(s, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+    
+    sv.sin_family = AF_INET;
+    sv.sin_port = htons(p);
+    sv.sin_addr.s_addr = INADDR_ANY;
+    sv.sin_len = sizeof sv;
+
+    svz = sizeof (sv);
+    int b = bind(s, (struct sockaddr*) &sv, svz);
+    if (b < 0) error(bind_err);
+
+    int l = listen(s, max_peers);
+    if (l < 0) error(lis_err);
+
+    while (ts) {
+
+        cvz = sizeof (cv);
+        c = accept(s, (struct sockaddr*) &cv, cvz);
+        if (c < 0) error(acc_err);
+
+        r = send(c, buf, sizeof(buf), 0);
+        if (r < 0) error(read_err);
+
+        w = recv(c, buf, strlen(buf), 0);
+        if (w < 0) error(write_err);
+    }
+}
