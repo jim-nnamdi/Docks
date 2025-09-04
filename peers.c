b58caac766904a9068eadc2b9598686a63b24557
Jim <jimsamuel50@gmail.com>
2025-09-04 23:44:09 +0100

    listen to peers incoming connections in the network

---
+++ peers.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/event.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include "constants.h"
+
+int listen_for_peers(int p) {
+    int s, c;
+    size_t rcv;
+    char buf[BUFSIZ];
+    struct sockaddr_in sv,cv;
+    socklen_t svz, cvz;
+
+    s = socket(AF_INET, SOCK_DGRAM, 0);
+    if (s < 0) error(bcast_sock_err);
+
+    sv.sin_family = AF_INET;
+    sv.sin_port = htons(p);
+    sv.sin_addr.s_addr = INADDR_ANY;
+
+    svz = sizeof(sv);
+    int b =  bind(s, (struct sockaddr*)&sv, svz);
+    if (b < 0) error(bind_err);
+
+    while (ts) {
+        cvz = sizeof(cv);
+        rcv = recvfrom(s, buf, sizeof(buf) - 1, 0, (struct sockaddr*)&cv, cvz);
+        if (rcv < 0) error(recv_err); 
+        buf[rcv] = 0;
+        if (strncmp(buf, broadcast_single_peer, strlen(buf)) == 0)
+            dock_df(inet_ntoa(cv.sin_addr));
+    }
+
+    close(s);
+}
