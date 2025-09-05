d7445c424b396d6111e761d78fbb984ac69b5080
Jim <jimsamuel50@gmail.com>
2025-09-05 09:24:47 +0100

    memset for sockaddr and buffers

--- peers.c
+++ peers.c
@@ -10,6 +10,7 @@ int listen_for_peers(int p) {
     s = socket(AF_INET, SOCK_DGRAM, 0);
     if (s < 0) error(bcast_sock_err);
 
+    memset(&sv, 0, sizeof(sv));
     sv.sin_family = AF_INET;
     sv.sin_port = htons(p);
     sv.sin_addr.s_addr = INADDR_ANY;
@@ -19,6 +20,7 @@ int listen_for_peers(int p) {
     if (b < 0) error(bind_err);
 
     while (ts) {
+        memset(buf, 0, sizeof(buf));
         cvz = sizeof(cv);
         rcv = recvfrom(s, buf, sizeof(buf) - 1, 0, (struct sockaddr*)&cv, cvz);
         if (rcv < 0) error(recv_err); 
