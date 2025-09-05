60b69a058d3dc4ab9884bd183a173a5f13cc6336
Jim <jimsamuel50@gmail.com>
2025-09-05 07:17:26 +0100

    work on constants file & external declarations

---
+++ constants.c
@@ -0,0 +1,17 @@
+#include "constants.h"
+
+const char *svr_err = "no such server";
+const char *socket_err = "socket error";
+const char *bind_err = "bind error";
+const char *lis_err = "listen error";
+const char *acc_err = "accept error";
+const char *read_err = "read error";
+const char *write_err = "write error";
+const char *connect_err = "connect error";
+const char *recv_err = "receive_from error";
+const char *connect_err = "sendto error";
+
+const char *bcast_sock_err = "broadcast socket error";
+const char *bcast_sendto_error = "broadcast send to error";
+const char *BCAST_PUB_ADDR = "255.255.255.255";
+const char *broadcast_single_peer = "looking for peers";
